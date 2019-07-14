ed4579a74726cc077f4d983edc3b366b8f7a38ab
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2019-07-14 23:44:31 +0700

    1907

--- api/comment/comment.controller.js
+++ api/comment/comment.controller.js
@@ -1,13 +1,36 @@
-const Course = require('./../course/course.model')
 const User = require('./../users/users.model')
-const Lesson = require('./lesson.model')
+const Comment = require('./comment.model')
 
-module.exports ={
-    create: async (req,res)=> {
+module.exports = {
+    create: async (req, res) => {
+        // ! Validate request
+        if (!req.body.content) {
+            return res.status(400).send({
+                message: "Comment can not be empty"
+            });
+        }
+
+        // ! Create a Comment
         try {
+            let newComment = {
+                time: Date.now(),
+                content: req.body.content
+            }
+            let comment = await Comment.create(newComment);
 
-        } catch (error) {
-            res.status(500).send('There was a problem adding the information to the database.')
+            await User.findOneAndUpdate({
+                user: req.user.data._id
+            })
+
+            res.status(201).json({
+                success: true,
+                msg: "Success create a Comment",
+                data: comment
+            })
+        } catch (error) { // ? show error
+            res.status(500).send({
+                message: error.message || "Some error occurred while creating the Comment."
+            });
         }
     }
 }
