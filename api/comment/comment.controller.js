d4a82ebec4f77064da386bf7530b4edd194d1690
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2019-07-15 10:46:35 +0700

    update cmt

--- api/comment/comment.controller.js
+++ api/comment/comment.controller.js
@@ -23,20 +23,45 @@ module.exports = {
                 user: req.user.data._id
             })
 
-            await Lesson.findOneAndUpdate(
-                {
-                    _id: req.params.idLesson
-                },
-                {
-                    $push: {comment: comment._id}
+            await Lesson.findOneAndUpdate({
+                _id: req.params.idLesson
+            }, {
+                $push: {
+                    comment: comment._id
                 }
-            )
+            })
 
             res.status(201).json({
                 success: true,
                 msg: "Success create a Comment",
                 data: comment
             })
+        } catch (error) { //TODO: Show error
+            res.status(500).send({
+                message: error.message || "Some error occurred while creating the Comment."
+            });
+        }
+    },
+    update: async (res, req) => {
+        //TODO: Validate request
+        if (!req.body.content) {
+            return res.status(400).send({
+                message: "Comment can not be empty"
+            });
+        }
+        //TODO: Create a Comment
+        try {
+            let update = JSON.parse(req.body.content);
+            Comment.findByIdAndUpdate(req.params.idComment, update, (err, result) => {
+                if (err) {
+                    error.message || "Some error occurred while creating the Comment."
+                }
+                resres.status(201).json({
+                    success: true,
+                    msg: "Success create a Comment",
+                    data: result
+                })
+            });
         } catch (error) { //TODO: Show error
             res.status(500).send({
                 message: error.message || "Some error occurred while creating the Comment."
