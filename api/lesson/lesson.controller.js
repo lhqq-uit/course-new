c1ce258bd3bce7ff8ea080ea30623046ed1c648b
QuachPhuQuoc <quachphuquoc@gmail.com>
2019-07-15 18:07:22 +0700

    update lesson & quiz, no used upload muliter

--- api/lesson/lesson.controller.js
+++ api/lesson/lesson.controller.js
@@ -7,7 +7,8 @@ module.exports ={
         try {
             let newLesson ={
                 title: req.body.title,
-                description: req.body.description,
+                desciption: req.body.description,
+                //document: req.file.filename,
                 course: req.params.idCourse
             }
 
@@ -38,6 +39,86 @@ module.exports ={
             })
         } catch (error) {
             res.status(500).send('There was a problem adding the information to the database.')
+        }
+    },
+    update: async (req,res) => {
+        try {
+            let newLesson = {
+                title: req.body.title,
+                desciption: req.body.description,
+                //document: req.file.filename,
+            }
+
+            let lesson = await Lesson.findByIdAndUpdate(req.params.idLesson,newLesson);
+            // let lesson = await Lesson.findOneAndUpdate({_id: req.params.idLesson}, newLesson);
+            if(!lesson) return res.status(404).send('No course found or you are not author of lesson');
+            console.log(lesson)
+            res.status(201).json({
+                success: true,
+                msg : "Success update a lesson",
+                data: newLesson
+            })
+        } catch (error) {
+            res.status(500).send('There was a problem updating the information to the database.')
+        }
+    },
+    getOneLesson: async (req, res) => {
+        try {
+            let lesson = await Lesson.findById(req.params.idLesson)
+            res.status(201).json({
+                success: true,
+                msg: "Success get a lesson",
+                data: lesson
+            })
+        } catch (error) {
+            res.status(404).json({message: 'No lesson found'})
+        }
+    },
+    getAllLesson : async (req, res) => {
+        let lesson = await Lesson.find({},(err, data)=>{
+            if(err) return res.status(404).send('No lesson found');
+            res.status(201).json({
+                success: true,
+                msg: "Success get all lesson",
+                data: lesson
+            });
+         });
+    },
+    delete : async (req, res) => {
+        try {
+            let lesson = await Lesson.findByIdAndDelete(
+                {
+                    _id: req.params.idLesson
+                }
+            );
+            //if(!lesson) return res.status(404).json('No course found or you are not author of lesson');
+    
+            await Course.findOneAndUpdate(
+                {
+                    lesson: req.params.idLesson
+                },
+                {
+                    $pull: {lessons:req.params.idLesson}
+                }
+            );
+    
+            await Teacher.findOneAndUpdate(
+                {
+                    user: req.user.data._id
+                },
+                {
+                    $inc: {iq: -100}
+                }
+            );
+    
+
+            
+            res.status(201).json({
+                success: true,
+                msg: "Success delete a lesson",
+            })
+        } catch (error) {
+            res.status(500).json('There was a problem deleting the information to the database.')
         }
     }
 }
--- api/lesson/lesson.routes.js
+++ api/lesson/lesson.routes.js
@@ -5,7 +5,34 @@ const router = express.Router();
 const lessonController = require('./lesson.controller');
 const multer = require('multer')
 const checkPermission = require('../../helper/CheckPermission');
+var storage =   multer.diskStorage({
+    // file upload destination
+    destination: function (req, file, callback) {
+      callback(null, './../public/upload');
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.fieldname + '-' + Date.now());
+    }
+  });
+  var upload = multer({ storage : storage}).fields([
+      {
+          name: 'video',
+          name: 'document'
+      }
+  ])
 
+//create lesson
 router.post('/:idCourse/',  passport.authenticate('jwt', { session: false}), checkPermission.isTeacher, lessonController.create);
 
+//update lesson
+router.put('/:idLesson', passport.authenticate('jwt', { session: false}), checkPermission.isTeacher, lessonController.update);
+
+//get a lesson
+router.get('/:idLesson', lessonController.getOneLesson);
+
+//get all lesson
+router.get('/',lessonController.getAllLesson);
+
+//delete lesson
+router.delete('/:idLesson', passport.authenticate('jwt', { session: false}), checkPermission.isTeacher, lessonController.delete);
 module.exports = router;
